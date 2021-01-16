dbd2447ad10de9dcb8b543cc5f0f768b034e9d53
jhigger <njohnjhigger@gmail.com>
2021-01-16 19:45:13 +0800

    Added bottom navigation bar
    Signed-off-by: jhigger <njohnjhigger@gmail.com>

--- app/src/main/java/com/example/occupines/MainActivity.java
+++ app/src/main/java/com/example/occupines/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.occupines;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirstFragment firstFragment = new FirstFragment();
+        SecondFragment secondFragment = new SecondFragment();
+        ThirdFragment thirdFragment = new ThirdFragment();
+        FourthFragment fourthFragment = new FourthFragment();
+        FifthFragment fifthFragment = new FifthFragment();
+
+        setCurrentFragment(firstFragment);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    setCurrentFragment(firstFragment);
+                    return true;
+                case R.id.likes:
+                    setCurrentFragment(secondFragment);
+                    return true;
+                case R.id.rentals:
+                    setCurrentFragment(thirdFragment);
+                    return true;
+                case R.id.calendar:
+                    setCurrentFragment(fourthFragment);
+                    return true;
+                case R.id.notifications:
+                    setCurrentFragment(fifthFragment);
+                    return true;
+            }
+            return false;
+        });
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.flFragment, fragment).
+                commit();
     }
 }
