9d6c9603a14914344391c8fc6347727db8063415
jhigger <njohnjhigger@gmail.com>
2021-01-22 20:23:50 +0800

    Added calendar
    Signed-off-by: jhigger <njohnjhigger@gmail.com>

--- app/src/main/java/com/example/occupines/FourthFragment.java
+++ app/src/main/java/com/example/occupines/FourthFragment.java
@@ -4,48 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FourthFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FourthFragment extends Fragment {
+import java.util.Calendar;
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class FourthFragment extends Fragment {
 
     public FourthFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FourthFragment.
-     */
-    public static FourthFragment newInstance(String param1, String param2) {
-        FourthFragment fragment = new FourthFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            String mParam1 = getArguments().getString(ARG_PARAM1);
-            String mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        CalendarView calendarView = view.findViewById(R.id.calendarView);
+        setDate(calendarView, "3/26/2015");
+    }
+
+    public void setDate(CalendarView calendarView, String date) {
+        String[] parts = date.split("/");
+
+        int month = Integer.parseInt(parts[0]);
+        int day = Integer.parseInt(parts[1]);
+        int year = Integer.parseInt(parts[2]);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        long milliTime = calendar.getTimeInMillis();
+
+        calendarView.setDate(milliTime);
     }
 
     @Override
