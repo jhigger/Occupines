3b849f8db470982040698bcf981a5ecf902a5c87
John Jhigger <njohnjhigger@gmail.com>
2021-01-27 14:24:57 +0800

    Added image compressor
    Signed-off-by: jhigger <njohnjhigger@gmail.com>

--- app/src/main/java/com/example/occupines/ProfileFragment.java
+++ app/src/main/java/com/example/occupines/ProfileFragment.java
@@ -30,8 +30,13 @@ import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Objects;
+
+import id.zelory.compressor.Compressor;
 
 
 public class ProfileFragment extends Fragment {
@@ -138,16 +143,45 @@ public class ProfileFragment extends Fragment {
 
     private void uploadImage() {
         StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-        StorageReference pathReference = storageRef.child("images").child(Objects.requireNonNull(mAuth.getUid())).child("profile"); //images/User id/profile.jpg
-        UploadTask uploadTask = pathReference.putFile(imagePath);
+        //images/User id/profile.jpg
+        StorageReference pathReference = storageRef.child("images").child(Objects.requireNonNull(mAuth.getUid())).child("profile");
+        //Compress image then upload
+        UploadTask uploadTask = pathReference.putFile(compressImage(imagePath));
         uploadTask
                 .addOnSuccessListener(taskSnapshot -> {
                     new Utility().showToast(getContext(), "Profile picture uploaded");
+                    //Restart activity to reload new uploaded image
                     Intent intent = Objects.requireNonNull(getActivity()).getIntent();
                     getActivity().finish();
                     startActivity(intent);
                 })
                 .addOnFailureListener(e -> new Utility().showToast(getContext(), "Error: Uploading profile picture"));
+    }
+
+    private Uri compressImage(Uri imagePath) {
+        //Getting imageUri and store in file. and compress to using compression library
+        File filesDir = Objects.requireNonNull(getContext()).getFilesDir();
+        File imageFile = new File(filesDir, "profile.jpg");
+
+        File compressedImage = null;
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(Objects.requireNonNull(getActivity()).getContentResolver(), imagePath);
+            OutputStream os = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.WEBP, 100, os);
+            os.flush();
+            os.close();
+
+            compressedImage = new Compressor(Objects.requireNonNull(getContext()))
+                    .setMaxWidth(250)
+                    .setMaxHeight(250)
+                    .setQuality(75)
+                    .setCompressFormat(Bitmap.CompressFormat.WEBP)
+                    .compressToFile(imageFile, "compressedImage");
+        } catch (Exception e) {
+            Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
+        }
+
+        return Uri.fromFile(compressedImage);
     }
 
     @Override
