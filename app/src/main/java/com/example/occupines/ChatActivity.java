dbf3a61452b0d353ed08e49556b66ddf0fe4114f
jhigger <njohnjhigger@gmail.com>
2021-02-11 02:26:00 +0800

    Added show chat messages
    Signed-off-by: jhigger <njohnjhigger@gmail.com>

--- app/src/main/java/com/example/occupines/ChatActivity.java
+++ app/src/main/java/com/example/occupines/ChatActivity.java
@@ -9,12 +9,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.NetworkPolicy;
@@ -22,12 +27,15 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
     private static final String TAG = "ChatActivity";
+    private static final String COLLECTION = "messages";
 
     private FirebaseUser currentUser;
     private FirebaseFirestore db;
@@ -37,6 +45,11 @@ public class ChatActivity extends AppCompatActivity {
     private TextView username;
     private ImageView userImage;
 
+    private MessageAdapter messageAdapter;
+    private List<Chat> chats;
+    private User user;
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +58,18 @@ public class ChatActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
         storageRef = FirebaseStorage.getInstance().getReference();
         loadingDialog = new LoadingDialog(this);
+
+        // 1. get a reference to recyclerView
+        recyclerView = findViewById(R.id.chatRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        // 2. set layoutManger
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        // this is data for recycler view
+        chats = new ArrayList<>();
+        // 5. set item animator to DefaultAnimator
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         username = findViewById(R.id.username);
         userImage = findViewById(R.id.userImage);
@@ -68,7 +93,6 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     private void getUserInfo(String userId) {
-        loadingDialog.start();
         db.collection("users").document(userId)
                 .get()
                 .addOnCompleteListener(task -> {
@@ -94,8 +118,11 @@ public class ChatActivity extends AppCompatActivity {
                                             .centerInside()
                                             .fit()
                                             .into(userImage);
+
                                     username.setText(document.getString("username"));
-                                    loadingDialog.dismiss();
+                                    user = new User(userId, document.getString("username"), localFile);
+
+                                    readMessages(currentUser.getUid(), userId, user);
                                 });
                                 if (localFile.delete()) {
                                     Log.d(TAG, "Temp file deleted");
@@ -126,5 +153,43 @@ public class ChatActivity extends AppCompatActivity {
                     Utility.showToast(this, "Error: Submission failed");
                     Log.w(TAG, "Error writing document", e);
                 });
+    }
+
+    private void readMessages(String myId, String userId, User user) {
+        loadingDialog.start();
+
+        db.collection(COLLECTION)
+                .orderBy("createdAt", Query.Direction.ASCENDING)
+                .addSnapshotListener((value, e) -> {
+                    if (e != null) {
+                        Log.w(TAG, "Listen failed.", e);
+                        return;
+                    }
+                    //Redraw on data change
+                    chats.clear();
+                    for (QueryDocumentSnapshot document : value) {
+                        Chat chat = new Chat(
+                                document.getString("sender"),
+                                document.getString("receiver"),
+                                document.getString("message")
+                        );
+                        if (chat.getReceiver().equals(myId) && chat.getSender().equals(userId) ||
+                                chat.getReceiver().equals(userId) && chat.getSender().equals(myId)) {
+                            chats.add(chat);
+                            // 3. create an adapter
+                            messageAdapter = new MessageAdapter(chats, user);
+                            // 4. set adapter
+                            recyclerView.setAdapter(messageAdapter);
+                        }
+                        loadingDialog.dismiss();
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        messageAdapter = null;
+        recyclerView.setAdapter(null);
+        super.onDestroy();
     }
 }
