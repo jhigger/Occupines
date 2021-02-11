2b5d2e08e6b9855155bce2538f029ab256a4735b
jhigger <njohnjhigger@gmail.com>
2021-02-11 12:01:49 +0800

    Replaced notification with location
    Signed-off-by: jhigger <njohnjhigger@gmail.com>

--- app/src/main/java/com/example/occupines/FifthFragment.java
+++ app/src/main/java/com/example/occupines/FifthFragment.java
@@ -4,24 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class FifthFragment extends Fragment {
 
-    String[] notifications;
-    ArrayList<String> list;
-    int added;
-
     public FifthFragment() {
-        notifications = new String[]{"1", "2", "3", "4", "5", "6", "7"};
-        list = new ArrayList<>(Arrays.asList(notifications));
-        added = getNotificationCount();
+
     }
 
     @Override
@@ -33,32 +22,9 @@ public class FifthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_fifth, container, false);
-        ListView listView = view.findViewById(R.id.notificationList);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.fragment_fifth, R.id.textNotifications, list);
-        listView.setAdapter(arrayAdapter);
-
-        listView.setOnItemClickListener((parent, view1, position, id) -> {
-
-            list.remove(position); // remove item at index in list data source
-            arrayAdapter.notifyDataSetChanged(); // call it for refresh ListView
-        });
-
-        added = 0;
-
+        View view;
+        view = inflater.inflate(R.layout.fragment_fifth, container, false);
+        // START HERE
         return view;
-    }
-
-    public int getNotificationCount() {
-        return list.size();
-    }
-
-    public int getAdded() {
-        return added;
-    }
-
-    public void addNotification(String message) {
-        list.add(message);
-        added += 1;
     }
 }
--- app/src/main/java/com/example/occupines/MainActivity.java
+++ app/src/main/java/com/example/occupines/MainActivity.java
@@ -2,17 +2,14 @@ package com.example.occupines;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.MutableLiveData;
 
 import com.example.occupines.databinding.ActivityMainBinding;
-import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
@@ -32,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -51,17 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Add badge on notification
         BottomNavigationView bottomNav = binding.bottomNavigationView;
-        BadgeDrawable badge = bottomNav.getOrCreateBadge(R.id.notifications);
-
-        //Number of notifications
-        MutableLiveData<Integer> number = new MutableLiveData<>();
-        //Initialize with a value
-        number.setValue(fifthFragment.getNotificationCount());
-        //Listener for variable
-        number.observe(MainActivity.this, integer -> {
-            destroyBadge(badge);
-            setupBadge(badge, number.getValue());
-        });
 
         //Show each fragment on each menu item click
         bottomNav.setOnNavigationItemSelectedListener(item -> {
@@ -74,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             } else if (itemId == R.id.likes) {
                 //2nd page
-                addNotification(fifthFragment, "Testing", number);
                 setCurrentFragment(secondFragment);
                 return true;
             } else if (itemId == R.id.messages) {
@@ -88,17 +72,10 @@ public class MainActivity extends AppCompatActivity {
             } else if (itemId == R.id.notifications) {
                 //5th page
                 setCurrentFragment(fifthFragment);
-                //Remove badge
-                destroyBadge(badge);
                 return true;
             }
             return false;
         });
-    }
-
-    private void addNotification(FifthFragment fifthFragment, String message, MutableLiveData<Integer> number) {
-        fifthFragment.addNotification(message);
-        number.postValue(fifthFragment.getAdded());
     }
 
     private void downloadImage(Fragment firstFragment) {
@@ -156,27 +133,6 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setMessage("Sign out?").setPositiveButton("Yes", dialogClickListener)
                 .setNegativeButton("No", dialogClickListener).show();
-    }
-
-    private void setupBadge(BadgeDrawable badge, int number) {
-        if (badge != null) {
-            if (number != 0) {
-                // An icon only badge will be displayed unless a number is set:
-                badge.setBackgroundColor(getResources().getColor(R.color.badge_color));
-                badge.setBadgeTextColor(Color.WHITE);
-                badge.setBadgeGravity(BadgeDrawable.TOP_END);
-                badge.setMaxCharacterCount(3);
-                badge.setNumber(number);
-                badge.setVisible(true);
-            } else {
-                destroyBadge(badge);
-            }
-        }
-    }
-
-    private void destroyBadge(BadgeDrawable badge) {
-        badge.clearNumber();
-        badge.setVisible(false);
     }
 
     @Override
